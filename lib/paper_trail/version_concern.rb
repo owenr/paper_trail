4f745671732c7a9f9aa7305629fe859a8081b62a
Owen Rodda <owen.rodda@gmail.com>
2015-12-31 00:12:02 -0500

    Set belongs_to .. :required => false only if this AR supports required by default

--- lib/paper_trail/version_concern.rb
+++ lib/paper_trail/version_concern.rb
@@ -5,7 +5,9 @@ module PaperTrail
     extend ::ActiveSupport::Concern
 
     included do
-      belongs_to :item, :polymorphic => true, :required => false
+      opts = {:polymorphic => true}
+      opts[:required] = false if ActiveRecord::Base.respond_to? :belongs_to_required_by_default
+      belongs_to :item, opts
 
       # Since the test suite has test coverage for this, we want to declare
       # the association when the test suite is running. This makes it pass when
